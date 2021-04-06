7dc1896d8e994a53d22f93cba8e107811720a6d6
Lauren <laurengibbonz@gmail.com>
2021-04-05 20:56:09 -0400

    project updates

--- src/components/Project.js
+++ src/components/Project.js
@@ -1,11 +1,14 @@
 import React from 'react';
 
-const Project = () => {
-    return (
-        <div className="item">
-            <h2>Project with Arrow Function</h2>
-        </div>
-    )
+const Project = ({projects}) => {
+    return projects.map((project, i) => {
+        return (
+            <div key={i} className="item">
+                <h2>{project.name}</h2>
+                <img src={project.img} alt={project.name} />
+            </div>
+        )
+    });
 }
 
 export default Project;
